3f621313635e9ee6de100382e606d5b0f38740cd
chaudharipa <chaudharipa@users.noreply.github.com>
2016-07-10 14:19:39 +0530

    Add files via upload

---
+++ Ardu_flow_Sensor_node/Ardu_flow_Sensor_node.ino
@@ -0,0 +1,136 @@
+//#include "ArduFlowMtrConstants.h"
+
+const char METER_1_DEVICE_ID[ ] = "WTR_PUN_00001_2016";
+const char METER_2_DEVICE_ID[ ] = "WTR_PUN_00002_2016";
+const char METER_3_DEVICE_ID[ ] = "WTR_PUN_00003_2016";
+const float arduinoRefVoltage = 4.75;//this is indeed Arduino reference voltage since analogRead will read voltage from target circuit as how many parts of Arduino voltage (out of 1023 parts)
+const float targetMaxVoltage = 5.06;//Maximum voltage of the circuit which is to be measured
+//float totalCircuitResistance = 0; //add up all the attached resistances in circuit in Ohms
+const int mAmVConversionFactor = 1000; //factor to convert from Amp to mA
+const int endResisterValue = 22; //fixed value of last resister (in Ohms) in circuit
+const int PIPE_FLOWRATE_CONV_FACTOR = 2; // this is the factor using which flow rate value is uniformly upgraded. Use across all sensors if flow rate dropping very low to be measured.
+const int MAX_FLOWRATE_IGNORE_LIMIT = 18;//below this limit if flow is flowing through supply line, Green LED will not light and user will not be informed.
+const int PIPE_LENGTH = 10;//10 meters of pipe length (represented by 1 meters of actual length
+const long READING_INTERVAL = 300000; //Take readings every 5 minutes. 1 min = 60000 milliseconds
+const int checkInterval = 10;//check if time interval reached and blink LED every checkInterval milliseconds
+
+//const int lastSensorPin = A0;
+const int meter1Pin1 = A0;
+const int meter1Pin2 = A1;
+const int meter2Pin1 = A2;
+const int meter2Pin2 = A3;
+const int meter3Pin1 = A4;
+const int meter3Pin2 = A5;
+//const int valveM1Pin = 8;
+const int meter1LEDg = 4;
+const int meter1LEDr = 5;
+const int meter2LEDg = 6;
+const int meter2LEDr = 7;
+const int meter3LEDg = 8;
+const int meter3LEDr = 9;
+const int NUM_SAMPLES = 100;//number of samples to be read from a pin to average the output voltage. Maintain it at 100 as otherwise it gives inconsistant results.
+
+unsigned long timer;
+boolean firstTime;
+
+
+void setup() {
+  pinMode(meter1LEDg, OUTPUT);
+  pinMode(meter1LEDr, OUTPUT);
+  pinMode(meter2LEDg, OUTPUT);
+  pinMode(meter2LEDr, OUTPUT);
+  timer = millis();
+  firstTime = true;
+  Serial.begin(57600);
+   
+}
+
+void loop() {
+
+  //get the flow meter readings
+  
+
+    float totalFlowRate = 0.00;
+    float meter1FlowRate = 0.00;
+    float meter2FlowRate = 0.00;
+    float meter3FlowRate = 0.00;
+    totalFlowRate = getTotalFlowRate(targetMaxVoltage, mAmVConversionFactor, PIPE_FLOWRATE_CONV_FACTOR);
+    meter1FlowRate = getMeterReading(meter1Pin1, meter1Pin2);
+    meter2FlowRate = getMeterReading(meter2Pin1, meter2Pin2);
+    meter3FlowRate = getMeterReading(meter3Pin1, meter3Pin2);
+    
+    
+    //check if there is indeed flow flowing through supply line and if so calculate reading and also start LED blinking
+    if(meter1FlowRate > MAX_FLOWRATE_IGNORE_LIMIT) {
+      //tap on, start green LED blinking sequence
+      stopLED(meter1LEDr);
+      blinkLED(meter1LEDg);
+      
+    } else {
+      stopLED(meter1LEDg);
+      startLED(meter1LEDr);
+    }
+  
+    if(meter2FlowRate > MAX_FLOWRATE_IGNORE_LIMIT) {
+      //tap on, start green LED blinking sequence
+      stopLED(meter2LEDr);
+      blinkLED(meter2LEDg);
+    } else {
+      stopLED(meter2LEDg);
+      startLED(meter2LEDr);
+    }
+
+    if(meter3FlowRate > MAX_FLOWRATE_IGNORE_LIMIT) {
+      //tap on, start green LED blinking sequence
+      stopLED(meter3LEDr);
+      blinkLED(meter3LEDg);
+    } else {
+      stopLED(meter3LEDg);
+      startLED(meter3LEDr);
+    }
+
+  //Print the output the serial (since we could not send in cloud in time of hackathon
+  Serial.println("-----------------------------");
+  Serial.print("Total Main Pipeline flow rate : ");
+  Serial.print(totalFlowRate);
+  Serial.println(" L/Min");
+  Serial.println("---------");
+  Serial.print("Meter no. 1, Meter/consumer no. ");
+  Serial.print(METER_1_DEVICE_ID);
+  Serial.print(" Reading: ");
+  Serial.print(meter1FlowRate);
+  Serial.println(" L/Min");
+  Serial.println("---------");
+  Serial.print("Meter no. 2, Meter/consumer no. ");
+  Serial.print(METER_1_DEVICE_ID);
+  Serial.print(" Reading: ");
+  Serial.print(meter2FlowRate);
+  Serial.println(" L/Min");
+  Serial.println("---------");
+  Serial.print("Meter no. 3, Meter/consumer no. ");
+  Serial.print(METER_3_DEVICE_ID);
+  Serial.print(" Reading: ");
+  Serial.print(meter3FlowRate);
+  Serial.println(" L/Min");
+  Serial.println("---------");
+
+   //sleep for an interval
+  delay(checkInterval);
+}
+
+
+
+void startLED(int LEDPin) {
+  digitalWrite(LEDPin, HIGH);
+}
+void stopLED(int LEDPin) {
+  digitalWrite(LEDPin, LOW);
+}
+void blinkLED(int LEDPin) {
+  digitalWrite(LEDPin, HIGH);
+  delay(1);
+  digitalWrite(LEDPin, LOW);
+  delay(1);
+  digitalWrite(LEDPin, HIGH);
+}
+
---
+++ Ardu_flow_Sensor_node2/getFlowMtrReading.ino
@@ -0,0 +1,80 @@
+/*
+ * library to read, calibrate and return Flow meter reading
+ */
+
+
+ float getMeterReading(int meterPinStart, int meterPinEnd) {
+  float totalFlowRate = 0.00;
+  unsigned long timeInterval = 0;
+  
+  //check if reading interval reached
+  if(firstTime) {
+    timeInterval = millis();
+  } else {
+    timeInterval = timer + READING_INTERVAL;
+  }
+  if(millis() >= timeInterval) {
+    //do calibration
+    int sample_count = 0;
+    unsigned long meterPin1ReadTot = 0;
+    unsigned long meterPin2ReadTot = 0;
+    
+    //read average analog value from meter pin (range is 0 to 1023)
+    meterPin1ReadTot = readMeterPin(meterPinStart);
+    meterPin2ReadTot = readMeterPin(meterPinEnd);
+    
+    float meterPin1Volt = ((float)meterPin1ReadTot/(float)NUM_SAMPLES)* targetMaxVoltage/1023;
+    float meterPin2Volt = ((float)meterPin2ReadTot/(float)NUM_SAMPLES)* targetMaxVoltage/1023;
+    
+    //calculate supply meter flow Rate
+    float meterFlowRate = getFlowRatePerMin(meterPin1Volt, meterPin2Volt, mAmVConversionFactor, PIPE_FLOWRATE_CONV_FACTOR);
+    return meterFlowRate;
+  }
+ }
+
+ int readMeterPin(int pinNo) {
+  delay(300);
+  int currSensorValue1 = 0;
+  unsigned long sensorValSum = 0;
+  for(int i = 0; i < NUM_SAMPLES; i++) {
+    int readVal = analogRead(pinNo);
+    sensorValSum = sensorValSum + readVal;
+  }
+  currSensorValue1 = sensorValSum/NUM_SAMPLES;
+  sensorValSum = 0;
+  return currSensorValue1;
+ }
+
+ float getFlowRatePerMin(float startVoltage, float endVoltage, int conversionFactor, int pipeFlowFactor) {
+  float flowRate = 0.00;
+  if((startVoltage-endVoltage) <= startVoltage) {
+    flowRate = convertToFlowRatePerMin(startVoltage, endVoltage, conversionFactor, pipeFlowFactor);
+  } else if(((startVoltage-endVoltage) > startVoltage)) {
+    if(startVoltage > 0) {
+      flowRate = convertToFlowRatePerMin(startVoltage, 0, conversionFactor, pipeFlowFactor);
+    } else {
+      flowRate = 0.00;
+    }
+  }  else {
+      flowRate = 0.00;
+    }
+
+    return flowRate; 
+}
+
+float convertToFlowRatePerMin(float beginVolt, float endVolt, int conversionFactor, int pipeFlowFactor) {
+  /*
+   * Heigan-Posielle equation is : Volumetric flow rate = (pie * r4 * (P-P0))/ 8*viscocity*length of pipe
+   * assumption is r = 1 meter
+   * Viscocity at 30 degree celcius = 0.798
+   * 
+   */
+   float flowRateVolt = (beginVolt-endVolt) * (float)conversionFactor * (float)pipeFlowFactor;
+   float waterFlowRate = (3.14 * 1 * flowRateVolt * 60)/(8 * 0.798 * 1);
+   return waterFlowRate;
+}
+
+float getTotalFlowRate(float targetMaxVoltage, int conversionFactor, int pipeFlowFactor) {
+ float flowRate = convertToFlowRatePerMin(targetMaxVoltage, 0.00, conversionFactor, pipeFlowFactor);
+}
+
